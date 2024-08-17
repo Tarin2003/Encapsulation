144dccaae2ac12f9f669d41b5a76eeef065ae045
Sadia Tarin <129206871+Tarin2003@users.noreply.github.com>
2024-08-17 22:59:41 +0600

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsulation;
+
+
+ 
+public class Test {
+    public static void main(String[] args) {
+        Encapsulation a = new Encapsulation();
+        a.setAge(18);
+        System.out.println(a.getAge());
+        a.setId(130);
+        System.out.println(a.getId()); 
+      
+     
+    }
+    
+}
